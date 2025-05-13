00cb039d18b25f2b79471e6dd6ade334dfab502b
Kevin Abedinpour <kea003@edu.zealand.dk>
2025-05-13 21:28:41 +0200

    Updated AnimalRepo and Animal pages

--- Repositories/AnimalRepo.cs
+++ Repositories/AnimalRepo.cs
@@ -41,14 +41,18 @@ namespace Eksamensprojekt___Gruppe_7.Repositories
             _animals.Add(new Animal(
                 name: "Trofast",
                 chipnumber: "4572",
-                text: "HaderEfternavn@hotmail.com",
+                text: "test",
                 picture: "Dyr1 (Trofast).png"));
             _animals.Add(new Animal(
                 name: "Misser",
                 chipnumber: "4571",
-                text: "HaderEfternavn@hotmail.com",
+                text: "test",
                 picture: "Dyr2 (Misser).png"));
-            _animals.Add(new Animal("Vovse", "FieProfil.png", "44528579", "BoatMomma@hotmail.com"));
+            _animals.Add(new Animal(
+                name: "Trofast",
+                chipnumber: "4572",
+                text: "test",
+                picture: "Dyr1 (Trofast).png"));
 
         }
     }
