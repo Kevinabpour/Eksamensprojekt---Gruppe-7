4303391fd3b9e8cf1315e5928e6777f32d475407
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-05-16 21:23:07 +0200

    aa

---
+++ Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using Eksamensprojekt___Gruppe_7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+// by Ahmed
+
+namespace Eksamensprojekt___Gruppe_7.Pages.Employees
+{
+    public class IndexModel : PageModel
+    {
+        public List <Employee> Employees { get; set; }
+        public void OnGet()
+        {
+            Employees = new List<Employee>
+            {
+                new Employee("Mads","Mads.png", "12344321", "Mads@ri.dk", "Ansvarlig" ),
+                new Employee("Maja","Maja.png", "12344330", "Maja@ri.dk", "Dyrepasser" ),
+                new Employee("Claus","Claus.png", "45875496", "--------", "Frivillig" )
+            };
+        }
+    }
+}
