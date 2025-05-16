6cf800f2cd9f4d88d77d15cf9d0a3a834207a6a0
Ahmed Hemdan <aha007@edu.zealand.dk>
2025-05-16 12:49:50 +0200

    qq

---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace Eksamensprojekt___Gruppe_7.Models
+
+    // by Ahmed
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string JobTitle { get; set; }
+
+        public Employee()
+        {
+            
+        }
+
+        public Employee(string name, string picture, string phoneNumber, string email, string jobTitle)
+        {
+            Name = name;
+            Picture = picture;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            JobTitle = jobTitle;
+
+        }
+    }
+}
