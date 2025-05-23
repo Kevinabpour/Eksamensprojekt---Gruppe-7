7686f7a0d8deb5fb80967ac97dae92cde41c7def
Kevin Abedinpour <kea003@edu.zealand.dk>
2025-05-24 01:44:30 +0200

    Added booking model (for animal booking)

---
+++ Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace Eksamensprojekt___Gruppe_7.Models
+{
+    public class Booking
+    {
+        public int AnimalId { get; set; }
+        public string VisitorName { get; set; }
+        public string VisitorType { get; set; }
+        public DateTime VisitDate { get; set; }
+        public string Note { get; set; }
+    }
+}
